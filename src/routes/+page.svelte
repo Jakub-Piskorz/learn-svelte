82942b64187f1c009ac838d4689f06145f584c47
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2026-01-24 23:57:52 +0100

    wip: module-federation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
+<script>
+	import { browser } from '$app/environment';
+
+	let remoteAppTarget;
+	if (browser) {
+		(async () => {
+			// @ts-ignore
+			const app = await import('remote/remote-app');
+			new app.default({
+				target: remoteAppTarget,
+			});
+		})();
+	}
+
+	let count = 0;
+</script>
+
 <h1>Welcome to your library project</h1>
 <p>My name is Jacob and this is my first Svelte app.</p>
 <a href="/next">Click here to view the next page</a>
+<div bind:this={remoteAppTarget} />
