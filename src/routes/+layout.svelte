1a32385c57c822db5413f7ef63fc353e7e7fa64f
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2026-02-13 20:31:34 +0100

    chore: styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,16 @@
 	// import '../app.css';
 	import '../layout.css';
 	import { resolve } from '$app/paths';
+	import logo from '$lib/images/logo.png';
 
 	const { children } = $props();
 </script>
-<div class="grid grid-rows-[50px_1fr_50px] min-h-dvh w-dvw">
-	<header class="flex flex-row gap-5 items-center shadow-md py-2 px-4 border-b bg-background">
-		<a class="text-lg font-bold" href={resolve("/")}>Protesty.pl</a>
+<div class="grid grid-rows-[100px_1fr_50px] min-h-dvh w-dvw">
+	<header class="flex flex-row gap-5 items-center py-2 px-4 border-b-6 mb-4 border-black bg-background">
+		<a class="text-3xl w-5xl m-auto relative font-bold flex h-full items-center" href={resolve("/")}>
+			<img src={logo} alt="logo" class="object-contain h-36 self-baseline absolute top-0 left-[-170px]" />
+			<div>Protesty.pl</div>
+		</a>
 	</header>
 	<main class="py-5 w-5xl m-auto h-full flex flex-col gap-2 items-start justify-items-start">
 		{@render children()}
