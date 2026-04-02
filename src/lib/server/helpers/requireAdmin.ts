5072a194888c1e817251d3a63c1dd90cf89badfc
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2026-04-03 01:46:27 +0200

    fix: requireAdmin error message

--- src/lib/server/helpers/requireAdmin.ts
+++ src/lib/server/helpers/requireAdmin.ts
@@ -2,9 +2,9 @@ import { error } from '@sveltejs/kit';
 import isAdmin from '$libServer/helpers/isAdmin';
 import whoAmI from '$libServer/helpers/whoAmI';
 
-export default async (errorMsg = 'Unauthorized access') => {
+export default async (errorMsg = 'Invalid or missing credentials"') => {
 	const me = await whoAmI(true);
-	if (!me) return error(401, "Invalid or missing credentials");
+	if (!me) return error(401, errorMsg);
 	const admin = await isAdmin(me);
 	if (!admin) return error(401, errorMsg);
 	return me;
