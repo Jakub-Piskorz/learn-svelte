b86eeff947ceb31499bc1f7387f4887d2df01f9b
Jakub Piskorz <jakub.piskorz@gazeta.pl>
2026-04-10 23:59:31 +0200

    chore: removed unnecessary tables

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import * as Card from '$lib/components/ui/card/index.js';
-	import * as Table from '$lib/components/ui/table/index.js';
+	// import * as Table from '$lib/components/ui/table/index.js';
 	import * as Select from '$lib/components/ui/select';
 	import { enhance } from '$app/forms';
 	import { Button } from '$lib/components/ui/button/index.js';
@@ -16,144 +16,147 @@
 	const user = $derived(data.user);
 	const organizers: FormData[] = $derived(
 		data.organizers.map(org => ({ id: String(org.id), value: org.name }))
-	)
+	);
 	const locations: FormData[] = $derived(
 		data.locations.map(location => ({ id: String(location.id), value: location.name }))
-	)
+	);
 	let newEventName = $state('');
 	let newEventDescription = $state('');
 	let newLocationName = $state('');
 	let newOrganizerName = $state('');
 	let selectedOrganizer: string | undefined = $state();
 	let selectedOrganizerName: string | undefined = $derived(
-		data.organizers.find(org => String(org.id) === selectedOrganizer)?.name || "Select an organizer",
+		data.organizers.find(org => String(org.id) === selectedOrganizer)?.name || 'Select an organizer'
 	);
 	let selectedLocation: string | undefined = $state();
 	let selectedLocationName: string | undefined = $derived(
-		data.locations.find(location => String(location.id) === selectedLocation)?.name || "Select a location",
+		data.locations.find(location => String(location.id) === selectedLocation)?.name || 'Select a location'
 	);
 </script>
 
 <h1 class="font-bold text-xl">Admin panel</h1>
 <div class="grid grid-cols-6 gap-5 mt-5 align-items-center justify-content-between">
 	<div class="max-h-59 col-span-3">
-		<div class="rounded-md border flex flex-col max-h-full overflow-y-auto">
-			<Table.Root>
-				<Table.Header class="sticky top-0 z-10 bg-white">
-					<Table.Row>
-						<Table.Head colspan={1} class="w-1/10">
-							<div>Location Id</div>
-						</Table.Head>
-						<Table.Head colspan={1} class="w-7/10">
-							<div>Location name</div>
-						</Table.Head>
-						<Table.Head colspan={1} class="w-2/10">
-						</Table.Head>
-					</Table.Row>
-				</Table.Header>
-				<Table.Body>
-					{#each data.locations as location (location.id)}
-						<Table.Row>
-							<Table.Cell class="cn-table-cell" colspan={1}>
-								{location.id}
-							</Table.Cell>
-							<Table.Cell class="cn-table-cell" colspan={1}>
-								{location.name}
-							</Table.Cell>
-							<Table.Cell class="cn-table-cell" colspan={1}>
-								<form method="post" action="?/delete-location" use:enhance>
-									<input type="hidden" name="id" value={location.id} required />
-									<Button type="submit" size="sm">Remove location</Button>
-								</form>
-							</Table.Cell>
-						</Table.Row>
-					{/each}
-				</Table.Body>
-			</Table.Root>
-		</div>
+		<!--
+				<div class="rounded-md border flex flex-col max-h-full overflow-y-auto">
+					<Table.Root>
+						<Table.Header class="sticky top-0 z-10 bg-white">
+							<Table.Row>
+								<Table.Head colspan={1} class="w-1/10">
+									<div>Location Id</div>
+								</Table.Head>
+								<Table.Head colspan={1} class="w-7/10">
+									<div>Location name</div>
+								</Table.Head>
+								<Table.Head colspan={1} class="w-2/10">
+								</Table.Head>
+							</Table.Row>
+						</Table.Header>
+						<Table.Body>
+							{#each data.locations as location (location.id)}
+								<Table.Row>
+									<Table.Cell class="cn-table-cell" colspan={1}>
+										{location.id}
+									</Table.Cell>
+									<Table.Cell class="cn-table-cell" colspan={1}>
+										{location.name}
+									</Table.Cell>
+									<Table.Cell class="cn-table-cell" colspan={1}>
+										<form method="post" action="?/delete-location" use:enhance>
+											<input type="hidden" name="id" value={location.id} required />
+											<Button type="submit" size="sm">Remove location</Button>
+										</form>
+									</Table.Cell>
+								</Table.Row>
+							{/each}
+						</Table.Body>
+					</Table.Root>
+		-->
 	</div>
-	<div class="max-h-59 col-span-3">
-		<SimpleTable
-			class="rounded-md border flex flex-col max-h-full overflow-y-auto"
-			rows={organizers}
-			columns={["Organizer Id", "Organizer name"]} />
-	</div>
-	<form method="post" action="?/create-location" class="col-span-2" use:enhance>
-		<Card.Root class="w-[300px]">
-			<Card.Header>
-				<Card.Title>Add new location</Card.Title>
-				<Card.Description>Locations are places, where events happen. (Protesty)</Card.Description>
-			</Card.Header>
-			<Card.Content class="grow-1">
-				<Input name="name" required type="text" bind:value={newLocationName} placeholder="Location name" />
-			</Card.Content>
-			<Card.Footer>
-				<Button type="submit" class="w-full">Add location</Button>
-			</Card.Footer>
-		</Card.Root>
-		{#if form?.missing}<p class="error">The location name provided</p>{/if}
-	</form>
+<!--</div>-->
+<div class="max-h-59 col-span-3">
+	<SimpleTable
+		class="rounded-md border flex flex-col max-h-full overflow-y-auto"
+		rows={organizers}
+		columns={["Organizer Id", "Organizer name"]} />
+</div>
+<form method="post" action="?/create-location" class="col-span-2" use:enhance>
+	<Card.Root class="w-[300px]">
+		<Card.Header>
+			<Card.Title>Add new location</Card.Title>
+			<Card.Description>Locations are places, where events happen. (Protesty)</Card.Description>
+		</Card.Header>
+		<Card.Content class="grow-1">
+			<Input name="name" required type="text" bind:value={newLocationName} placeholder="Location name" />
+		</Card.Content>
+		<Card.Footer>
+			<Button type="submit" class="w-full">Add location</Button>
+		</Card.Footer>
+	</Card.Root>
+	{#if form?.missing}<p class="error">The location name provided</p>{/if}
+</form>
 
-	<form method="post" action="?/create-event" class="col-span-2" use:enhance>
-		<Card.Root class="w-[300px]">
-			<Card.Header>
-				<Card.Title>Add new event</Card.Title>
-				<Card.Description>Events are center to this website. That's what people go to look for</Card.Description>
-			</Card.Header>
-			<Card.Content class="grow-1">
-				<form method="POST" class="flex flex-col gap-2">
-					<Input name="name" required type="text" bind:value={newEventName} placeholder="Event name" />
-					<Input name="description" class="h-20" type="text" bind:value={newEventDescription} placeholder="Event description" />
-					<Select.Root required type="single" name="selectedOrganizer" bind:value={selectedOrganizer}>
-						<Select.Trigger class="w-full">
-							{selectedOrganizerName}
-						</Select.Trigger>
-						<Select.Content>
-							<Select.Group>
-								<Select.Label>Organizers</Select.Label>
-								{#each organizers as organizer (organizer.id)}
-									<Select.Item value={organizer.id} label={organizer.value}>
-										{organizer.value}
-									</Select.Item>
-								{/each}
-							</Select.Group>
-						</Select.Content>
-					</Select.Root>
-					<Select.Root required type="single" name="selectedLocation" bind:value={selectedLocation}>
-						<Select.Trigger class="w-full">
-							{selectedLocationName}
-						</Select.Trigger>
-						<Select.Content>
-							<Select.Group>
-								<Select.Label>Locations</Select.Label>
-								{#each locations as location (location.id)}
-									<Select.Item value={location.id} label={location.value}>
-										{location.value}
-									</Select.Item>
-								{/each}
-							</Select.Group>
-						</Select.Content>
-					</Select.Root>
-				</form>
-			</Card.Content>
-			<Card.Footer>
-				<Button type="submit" class="w-full">Add Event</Button>
-			</Card.Footer>
-		</Card.Root>
-	</form>
+<form method="post" action="?/create-event" class="col-span-2" use:enhance>
+	<Card.Root class="w-[300px]">
+		<Card.Header>
+			<Card.Title>Add new event</Card.Title>
+			<Card.Description>Events are center to this website. That's what people go to look for</Card.Description>
+		</Card.Header>
+		<Card.Content class="grow-1">
+			<form method="POST" class="flex flex-col gap-2">
+				<Input name="name" required type="text" bind:value={newEventName} placeholder="Event name" />
+				<Input name="description" class="h-20" type="text" bind:value={newEventDescription}
+							 placeholder="Event description" />
+				<Select.Root required type="single" name="selectedOrganizer" bind:value={selectedOrganizer}>
+					<Select.Trigger class="w-full">
+						{selectedOrganizerName}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.Label>Organizers</Select.Label>
+							{#each organizers as organizer (organizer.id)}
+								<Select.Item value={organizer.id} label={organizer.value}>
+									{organizer.value}
+								</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+				<Select.Root required type="single" name="selectedLocation" bind:value={selectedLocation}>
+					<Select.Trigger class="w-full">
+						{selectedLocationName}
+					</Select.Trigger>
+					<Select.Content>
+						<Select.Group>
+							<Select.Label>Locations</Select.Label>
+							{#each locations as location (location.id)}
+								<Select.Item value={location.id} label={location.value}>
+									{location.value}
+								</Select.Item>
+							{/each}
+						</Select.Group>
+					</Select.Content>
+				</Select.Root>
+			</form>
+		</Card.Content>
+		<Card.Footer>
+			<Button type="submit" class="w-full">Add Event</Button>
+		</Card.Footer>
+	</Card.Root>
+</form>
 
-	<form method="post" action="?/create-organizer" class="col-span-2" use:enhance>
-		<Card.Root class="w-[300px]">
-			<Card.Header>
-				<Card.Title>Add new organizer</Card.Title>
-				<Card.Description>Organizers are organisations or private people who host events.</Card.Description>
-			</Card.Header>
-			<Card.Content class="grow-1">
-				<Input name="name" required type="text" bind:value={newOrganizerName} placeholder="Organizer name" />
-			</Card.Content>
-			<Card.Footer>
-				<Button type="submit" class="w-full">Add Organizer</Button>
-			</Card.Footer>
-		</Card.Root>
-	</form>
+<form method="post" action="?/create-organizer" class="col-span-2" use:enhance>
+	<Card.Root class="w-[300px]">
+		<Card.Header>
+			<Card.Title>Add new organizer</Card.Title>
+			<Card.Description>Organizers are organisations or private people who host events.</Card.Description>
+		</Card.Header>
+		<Card.Content class="grow-1">
+			<Input name="name" required type="text" bind:value={newOrganizerName} placeholder="Organizer name" />
+		</Card.Content>
+		<Card.Footer>
+			<Button type="submit" class="w-full">Add Organizer</Button>
+		</Card.Footer>
+	</Card.Root>
+</form>
 </div>
